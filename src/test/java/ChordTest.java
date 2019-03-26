8fa8a1937321b10e91a50f79ace2047cb408895c
Igor Kharakhordin <westracer1@gmail.com>
2019-03-26 20:23:50 +0700

    + Тесты для проверки типов аккордов

--- src/test/java/ChordTest.java
+++ src/test/java/ChordTest.java
@@ -1,6 +1,8 @@
-import org.junit.jupiter.api.Test;
-
+import model.ChordRoot;
+import model.ChordType;
 import model.Chord;
+
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +10,17 @@ class ChordTest {
     @Test
     void classExists() {
         assertNotNull(Chord.class, "Класс Chord не существует");
+    }
+
+    @Test
+    void rootEnumCheck() {
+        assertNotNull(ChordRoot.class, "Enum ChordRoot не существует");
+        assertArrayEquals(ChordRoot.values(), CHORD_ROOTS, "Значения ChordRoot не совпадают");
+    }
+
+    @Test
+    void typeEnumCheck() {
+        assertNotNull(ChordRoot.class, "Enum ChordType не существует");
+        assertArrayEquals(ChordType.values(), CHORD_TYPES, "Значения ChordType не совпадают");
     }
 }
