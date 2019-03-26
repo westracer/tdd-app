d03b13a779c9bd014f19dc70dba7b3147cd839cc
Igor Kharakhordin <westracer1@gmail.com>
2019-03-26 19:59:33 +0700

    + junit 5, тест на существование класса

---
+++ src/test/java/ChordTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import model.Chord;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChordTest {
+    @Test
+    void classExists() {
+        assertNotNull(Chord.class, "Класс Chord не существует");
+    }
+}
