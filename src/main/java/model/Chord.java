052631c38d81d2531684e5083fceef64af14af5a
Igor Kharakhordin <westracer1@gmail.com>
2019-03-26 20:44:08 +0700

    + Конструктор и поля Chord

--- src/main/java/model/Chord.java
+++ src/main/java/model/Chord.java
@@ -1,5 +1,11 @@
 package model;
 
 public class Chord {
+    public ChordRoot root;
+    public ChordType type;
 
+    public Chord(ChordRoot root, ChordType type) {
+        this.root = root;
+        this.type = type;
+    }
 }
