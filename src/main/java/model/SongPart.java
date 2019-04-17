d002e8840648a84185f1bed7dc2bdc7ee99f4db6
Igor Kharakhordin <westracer1@gmail.com>
2019-04-17 17:46:04 +0700

    + Генерация части песни

--- src/main/java/model/SongPart.java
+++ src/main/java/model/SongPart.java
@@ -1,9 +1,28 @@
 package model;
 
 public class SongPart {
-    public ChordGroup[] chordGroups;
+    public static final int MIN_SENTENCES = 6;
+    public static final int MAX_SENTENCES = 15;
+    static final double SKIP_CHORD_SENTENCE = .15;
 
-    public SongPart(ChordGroup[] chordGroups) {
+    public String name;
+    public ChordGroup[] chordGroups;
+    public String[] sentences;
+
+    public SongPart(String n, ChordGroup[] chordGroups) {
+        this.name = n;
         this.chordGroups = chordGroups;
+
+        Random r = new Random();
+        sentences = new String[r.nextInt(MAX_SENTENCES - MIN_SENTENCES) + MIN_SENTENCES];
+        RandomSentences.Length l = r.nextBoolean() ?
+                RandomSentences.Length.MEDIUM :
+                r.nextBoolean() ?
+                        RandomSentences.Length.MEDIUM :
+                        RandomSentences.Length.SHORT;
+
+        for (int i = 0; i < sentences.length; i++) {
+            sentences[i] = RandomSentences.generateRandomSentence(l);
+        }
     }
 }
